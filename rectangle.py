719b99f8dbe7f93645d73e965f29b7ea2fc69d61
Veena Kuruba <javavk2022@gmail.com>
2025-07-28 14:26:44 -0700

    rect

--- rectangle.py
+++ rectangle.py
@@ -1,10 +1,5 @@
-rows=int(input("enter rows: "))
-columns=int(input("enter columns: "))
-symbol=input("enter symbol: ")
-for x in range (rows):
-        for y in range(columns):
-                print(symbol,end="")
-        print()
-    
-        
-        
+print(" -----------")
+print(" |          |")
+print(" |          |")
+print(" -----------")
+
