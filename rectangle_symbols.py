0a907f974cf7c5bc206f7ac92d5935e96f599948
Veena Kuruba <javavk2022@gmail.com>
2025-07-28 14:26:16 -0700

    rectangle_symbols.py

---
+++ rectangle_symbols.py
@@ -0,0 +1,10 @@
+rows=int(input("enter rows: "))
+columns=int(input("enter columns: "))
+symbol=input("enter symbol: ")
+for x in range (rows):
+        for y in range(columns):
+                print(symbol,end="")
+        print()
+    
+        
+        
